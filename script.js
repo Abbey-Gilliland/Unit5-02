adc2f5685e09a554c06cc5d94df24b54da41ee50
Abbey Gilliland <abbey.gilliland@ocsbstudent.ca>
2021-03-16 15:13:34 +0000

    fixed javascript.

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // This code makes an alert pop up to greet the viewer
-document.getElementById('text-button').addEventListener('click', function(){alert("Hello, World!"); });
+document.getElementById('text-button').addEventListener('click', myfunction) 
+
+function myfunction () {
+  alert("Hello, World!"); 
+}
